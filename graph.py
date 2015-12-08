3c115d38a3e81f94f0061bf1c575b9c3fb462252
technocake <technocake@komsys.org>
2015-12-08 23:29:15 +0100

    Mmmhmmm O said, and this version contains an argument parser

--- graph.py
+++ graph.py
@@ -1,12 +1,17 @@
+#!/usr/bin/env python
 #coding: utf-8
 import graphviz
 import re
 import requests
+import requests_cache
 from bs4 import BeautifulSoup
 import sys
+import argparse
 
 attendees = {}
 globallabels = {}
+VIEW_OUTPUT_SETTING = True
+OUTPUT_FOLDER = ""
 
 def fetchLinks(iterable):
 	vask = lambda x: re.search(r"http[^\s]+", x).group(0).rstrip()
@@ -19,16 +24,21 @@ def guid(link):
 		globallabels[link] = str(len(globallabels)+1)
 	return globallabels[link]
 
+def load_text(text):
+	attendees = {}
+	for attendee in text.split('#')[1:]:
+			lines = attendee.split("\n")
+			name = lines[0].rstrip()
+			links = fetchLinks(lines)
+			attendees[name] = links
+	return attendees
+
 
 def load_file(filename):
 	attendees = {}
 	# Henter ut urler fra tekstfilen
 	with open(filename) as datafile:
-		for attendee in datafile.read().split('#')[1:]:
-			lines = attendee.split("\n")
-			name = lines[0].rstrip()
-			links = fetchLinks(lines)
-			attendees[name] = links
+		attendees = load_text(datafile.read())
 	return attendees
 
 
@@ -46,6 +56,8 @@ def fetchInfo(links):
 				soup = BeautifulSoup(r.text, 'html.parser')
 				try:
 					title = soup.select("title")[0].string
+					meta = fetchMeta(soup)
+					#print meta
 				except:
 					title=""
 			else:
@@ -54,6 +66,11 @@ def fetchInfo(links):
 			nodes.append((label, title, link))
 	return nodes
 
+
+
+def fetchMeta(soup):
+	"Fetches metainfo from a html soup"
+	return soup.findAll(attrs={"name":"description"}) + soup.findAll(attrs={"name":"keyword"})
 
 def makeGraph(name, nodes):
 	dot = graphviz.Digraph(comment='Learning Path %s' %name)
@@ -79,7 +96,7 @@ def draw_learning_paths(attendees):
 		all_nodes.append(nodes)
 		fn = u"%s" % name
 		g = makeGraph(name, nodes)
-		g.render(name, view=True)
+		g.render(name, view=VIEW_OUTPUT_SETTING, directory=OUTPUT_FOLDER)
 	return all_nodes
 
 
@@ -96,25 +113,44 @@ def draw_agregated_graph(all_nodes, name):
 				next_label, next_title, next_link = nodes[i+1]
 				G.edge(label, next_label)
 
-	G.render("%s-Agregated_Graph"%(name), view=True)
+	G.render("%s-Agregated_Graph"%(name), directory=OUTPUT_FOLDER, view=VIEW_OUTPUT_SETTING)
 
-
-def usage():
-	return """
-		Usage:	%s inputfile.txt
-	"""
 
 def make_graphs(filename):
+
 	attendees = load_file(filename)
 	# One side effect not enough? How about two for you!
 	all_nodes = draw_learning_paths(attendees)
 	draw_agregated_graph(all_nodes, filename)
 
 
+def init(filename):
+	requests_cache.install_cache(filename)
+
+
 if __name__ == '__main__':
-	if len(sys.argv) < 2:
-		print usage()
-		exit(1)
-	filename = sys.argv[1]
-	make_graphs(filename)
+	parser = argparse.ArgumentParser(description='Make Learning Path Graphs.')
+	
+	parser.add_argument(
+		'files', nargs='+',
+				 help="The file(s) to process")
+	parser.add_argument(
+		'--folder', 
+				help='Set output folder for generated graphviz files',
+				default='')
+	parser.add_argument(
+		'--silent', action="store_true", 
+				help='If provided, the generated pdfs will not be displayed (default behavior)')
+	# Get the arguments
+	args = parser.parse_args()
+	VIEW_OUTPUT_SETTING = args.silent
+	OUTPUT_FOLDER = args.folder
+	
+
+	# Cache it.
+	for filename in args.files:
+		# Lets not waste bandwith, cache all web requests.
+		requests_cache.install_cache(filename)
+		
+		make_graphs(filename)
 	
